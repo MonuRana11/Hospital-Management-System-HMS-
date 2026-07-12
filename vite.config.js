e397702ac3eb024c89a1ae164a8e84dbf0cfd070
MonuRana11 <mt763452@gmail.com>
2026-07-12 22:40:51 +0530

    Dasboard

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,        // frontend port
+    port: 5174,        // dashboard port
   }
 })
